4d48126f1381c9b2b1d98797b43a94c3720290ac
Kenan Ozdamar <kenan.ozdamar@gmail.com>
2016-03-23 20:51:48 -0700

    This project creates a falling icicles game. This is the last assignment of udacity405 2D Game Development with libGDX. Initial commit.

---
+++ html/src/com/kenan/libgdxtutorial/icicles/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.kenan.libgdxtutorial.icicles.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.kenan.libgdxtutorial.icicles.IciclesGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new IciclesGame();
+        }
+}
